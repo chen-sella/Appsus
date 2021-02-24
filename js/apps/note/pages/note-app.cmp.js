6265c9c85702c9aa5ee9cccdaffc0dae288b1b40
chen-sella <chensella1@gmail.com>
2021-02-24 14:14:07 +0200

    changes after talk with tal

--- js/apps/note/pages/note-app.cmp.js
+++ js/apps/note/pages/note-app.cmp.js
@@ -1,6 +1,12 @@
+import notePreview from '../cmps/note-preview.cmp.js'
+
 export default {
   name: '',
   template: `
+      <section class="note-app">
+        <note-preview></note-preview>
+
+      </section>
               
           `,
   data() {
--- js/routes.js
+++ js/routes.js
@@ -1,24 +1,24 @@
-import appHome from './pages/app-home.cmp.js'
-import emailApp from './apps/email/pages/email-app.cmp.js'
-import emailDetails from './apps/email/pages/email-details.cmp.js'
-import noteApp from './apps/note/pages/note-app.cmp.js'
+import appHome from './pages/app-home.cmp.js';
+import emailApp from './apps/email/pages/email-app.cmp.js';
+import emailDetails from './apps/email/pages/email-details.cmp.js';
+import noteApp from './apps/note/pages/note-app.cmp.js';
 
 const routes = [
   {
-      path: '/',
-      component: appHome,
-  },
-  {
-    path: '/email',
-    component: emailApp,
+    path: '/',
+    component: appHome,
   },
   // {
   //     path: '/about',
   //     component: about
   // },
   {
-      path: '/kepp',
-      component: noteApp
+    path: '/keep',
+    component: noteApp,
+  },
+  {
+    path: '/email',
+    component: emailApp,
   },
   {
     path: '/email/:emailId',
@@ -27,4 +27,3 @@ const routes = [
 ];
 
 export const myRouter = new VueRouter({ routes });
- 
