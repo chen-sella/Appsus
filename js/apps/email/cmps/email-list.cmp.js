fd06ab85532d1a120e1ccc714612beecbf7c1870
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-24 18:04:58 +0200

    starting to style email page

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -5,7 +5,7 @@ export default {
     props: ['emails'],
     template: `
             <ul class="email-list-container">
-              <li v-for="email in emails" :key="email.id">
+              <li v-for="email in emails" :key="email.id" class="clean-list">
                 <email-preview :email="email"/>
               </li>
             </ul>   
--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -2,10 +2,13 @@ export default {
     name: 'emailPreview',
     props: ['email'],
     template: `
-            <section>
-              <h2>Name: {{email.sender}}</h2>
-              <h2>Title: {{email.subject}}</h2>
-              <p>Body: {{email.body}}</p>
+            <section class="email-preview-container flex">
+              <div class="name-circle"></div>
+              <div class="email-preview-details flex column space-between">
+                <h4>{{email.sender}}</h4>
+                <h4>{{email.subject}}</h4>
+                <p>{{email.body}}</p>
+              </div>
             </section>
         `,
     data() {
