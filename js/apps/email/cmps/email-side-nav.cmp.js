fe23ad61f501b585d7daeb811099a23ef0373d4f
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-24 17:25:44 +0200

    filtering by folders

---
+++ js/apps/email/cmps/email-side-nav.cmp.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'emailSideNav',
+    template: `
+            <nav class="email-side-nav-container flex column">
+                <a class="nav-link" @click="setFilter('inbox')">Inbox</a>
+                <a class="nav-link" @click="setFilter('starred')">Starred</a>
+                <a class="nav-link" @click="setFilter('sent')">Sent</a>
+                <a class="nav-link" @click="setFilter('trash')">Trash</a>
+            </nav>
+        `,
+    data() {
+      return {};
+    },
+    methods: {
+        setFilter(filterBy){
+            console.log('filterBy',filterBy);
+            this.$emit('filtered', filterBy);
+        }
+    },
+    computed: {},
+    components: {
+    },
+  };
--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -1,27 +1,45 @@
 import { emailService } from '../services/email.service.js';
 import emailList from '../cmps/email-list.cmp.js';
+import emailSideNav from '../cmps/email-side-nav.cmp.js';
 
 export default {
-  name: '',
+  name: 'emailApp',
   template: `
-          <section>
-            <email-list :emails="emails"/>
+          <section class="email-app-container flex">
+            <email-side-nav @filtered="setFilter"/>
+            <email-list v-if="emails" :emails="mailsToShow"/>
           </section>  
         `,
   data() {
     return {
       emails: null,
+      currFolder: 'inbox',
     };
   },
   methods: {
     loadEmails() {
       emailService.query()
-          .then(emails => this.emails = emails)
+          .then(emails => {
+            this.emails = emails
+            console.log('this.emails',this.emails);
+          })
+    },
+    setFilter(filterBy) {
+      this.currFolder = filterBy;
+      console.log('this.currFolder',this.currFolder);
+    }
   },
+  computed: {
+    mailsToShow() {
+      const emailsToShow = this.emails.filter(email => {
+          return email.folders.includes(this.currFolder)
+      })
+      return emailsToShow;
+  }
   },
-  computed: {},
   components: {
     emailList,
+    emailSideNav
   },
   created() {
     this.loadEmails();
