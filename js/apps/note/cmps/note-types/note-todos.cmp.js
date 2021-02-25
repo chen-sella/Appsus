4196425482a7b57bad95eb3e7fd59b70df7641da
chen-sella <chensella1@gmail.com>
2021-02-25 21:03:30 +0200

    edits

--- js/apps/note/cmps/note-types/note-todos.cmp.js
+++ js/apps/note/cmps/note-types/note-todos.cmp.js
@@ -1,11 +1,11 @@
 export default {
   name: 'noteTodos',
-  props: ['info'],
+  props: ['info', 'id'],
   template: `
       <section class="note-todos">
         <h3>{{info.label}}</h3>
         <ul>
-          <li v-for="todo in info.todos" :class="{marked: todo.doneAt}">{{todo.txt}}</li>
+          <li contenteditable="true" ref="txtPara" v-for="todo in info.todos" :class="{marked: todo.doneAt}" @keyup.enter="updateTxt">{{todo.txt}}</li>
         </ul>
       </section>
 
