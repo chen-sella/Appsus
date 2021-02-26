b786aee5fe257cb276d23ad7c608044e927f09c2
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-26 08:45:31 +0200

    no changes

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -4,11 +4,11 @@ import { eventBus } from '../../../services/event-bus.service.js';
 
 export default {
     name: 'emailList',
-    // props: ['emails','folder'],
     template: `
             <ul class="email-list-container">
               <li v-for="email in emails" :key="email.id" class="clean-list">
-                <router-link :to="'/email/'+folder/+email.id"><email-preview :email="email" @sendStarEvent="shareStarEvent"/></router-link>
+                <router-link :to="'/email/'+folder/+email.id"></router-link>
+                <!-- <router-link :to="'/email/'+folder/+email.id"><email-preview :email="email" @sendStarEvent="shareStarEvent"/></router-link> -->
                 <!-- <router-link :to="'/car/'+car.id">Details</router-link> -->
                 <!-- <router-link :to="'/car/edit/'+car.id">Edit</router-link> -->
 
@@ -27,8 +27,7 @@ export default {
       },
     },
     computed: {
-      
-      
+     
     },
     components: {
       emailPreview,
