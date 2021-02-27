ce790a16a26980297397df8f2893640337ca5885
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-27 03:12:52 +0200

    emitting the search

--- js/apps/email/cmps/email-filter.cmp.js
+++ js/apps/email/cmps/email-filter.cmp.js
@@ -2,8 +2,8 @@ export default {
     name: 'emailFilter',
     template: `
             <section class="email-filter-section flex">
-              <input type="search" placeholder="Search mail" v-model="strSearch">
-              <select name="" id="">
+              <input type="search" placeholder="Search mail" v-model="strSearch" @change="searchByStr">
+              <select name="readSort" v-model="readSearch" @change="sortByIsRead">
                 <option value="all">All</option>
                 <option value="read">Read</option>
                 <option value="unread">Unread</option>
@@ -13,11 +13,16 @@ export default {
     data() {
       return {
         strSearch: '',
-        searchBy: '',
+        readSearch: '',
       };
     },
     methods: {
-      
+      searchByStr(strSearch) {
+        this.$emit('strSortBy', strSearch);
+      },
+      sortByIsRead(readSearch) {
+        this.$emit('readSortBy', readSearch);
+      }
     },
     computed: {},
     components: {},
--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -7,7 +7,7 @@ export default {
   name: 'emailList',
   template: `
           <section class="email-list-container flex column">
-            <email-filter></email-filter>
+            <email-filter @strSortBy="sortByStr" @readSortBy="sortByRead"></email-filter>
             <ul v-if="emails" class="email-list">
               <li v-for="email in emailsToShow" :key="email.id" class="clean-list">
                 <email-preview :email="email" @sendStarEvent="updateFolder" @click.native="openDetails(email.id)" :folder="folder"></email-preview>
@@ -29,6 +29,12 @@ export default {
         this.emails = emails;
         this.mailsToShow();
       });
+    },
+    sortByStr(str) {
+      console.log('sorting by str...');
+    },
+    sortByStr(isRead) {
+      console.log('sorting by isRead...');
     },
     mailsToShow() {
       const emailsToShow = this.emails.filter((email) => {
